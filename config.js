c23f893dfe92a8e58f01af2b2177c03b7e4cf520
backmeupplz <backmeupplz@gmail.com>
2016-07-31 14:41:42 -0700

    remove dotrnv from dependencies

--- config.js
+++ config.js
@@ -2,7 +2,3 @@ module.exports = {
 	telegram_api_key: process.env.TELEGRAM_API_KEY,
 	database: process.env.MONGO_DB_URL
 };
-
-// 254748739:AAEsoVXQbrFvcMtijSm1SjSn8yp8NFrR9Jg
-// mongodb://localhost:27017/arbeit_bot
-// TELEGRAM_API_KEY='254748739:AAEsoVXQbrFvcMtijSm1SjSn8yp8NFrR9Jg' MONGO_DB_URL='mongodb://localhost:27017/arbeit_bot' node app.js
