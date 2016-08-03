95cca66714f93bb8f475c0ea8212814d4ba50a7a
backmeupplz <backmeupplz@gmail.com>
2016-08-03 00:43:47 -0700

    -m

--- helpers/telegramBot.js
+++ helpers/telegramBot.js
@@ -2,8 +2,8 @@
  * Used to initialize Telegam bot
  */
 
-let telegram = require('telegram-bot-api');
-let config = require('../config');
+const telegram = require('telegram-bot-api');
+const config = require('../config');
 
 module.exports = new telegram({
   token: config.telegram_api_key,
