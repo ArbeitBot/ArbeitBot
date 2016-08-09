690d7f57a0683de8ea932c98147431352698313f
backmeupplz <backmeupplz@gmail.com>
2016-08-08 17:41:38 -0700

    small fix of formatting

--- helpers/review.js
+++ helpers/review.js
@@ -10,23 +10,23 @@ const strings = require('./strings');
  * @param  {Telegram:Messager} msg Message received
  */
 eventEmitter.on(strings.askRateFreelancerInline, ({ msg, bot }) => {
-    const jobId = msg.data.split(strings.inlineSeparator)[1];
+  const jobId = msg.data.split(strings.inlineSeparator)[1];
 
-    dbmanager.findJobById(jobId, 'selectedCandidate client')
+  dbmanager.findJobById(jobId, 'selectedCandidate client')
     .then(job => {
-        const freelancer = job.selectedCandidate;
+      const freelancer = job.selectedCandidate;
 
-        let keyboard = keyboards.rateKeyboard(strings.rateClientInline, job._id);
-        let send = {
-            chat_id: job.current_inline_chat_id,
-            message_id: job.current_inline_message_id,
-            text: strings.rateClientMessage,
-            reply_markup: {
-                inline_keyboard: keyboard
-            }
-        };
-        send.reply_markup = JSON.stringify(send.reply_markup);
-        bot.editMessageText(send)
+      let keyboard = keyboards.rateKeyboard(strings.rateClientInline, job._id);
+      let send = {
+        chat_id: job.current_inline_chat_id,
+        message_id: job.current_inline_message_id,
+        text: strings.rateClientMessage,
+        reply_markup: {
+          inline_keyboard: keyboard
+        }
+      };
+      send.reply_markup = JSON.stringify(send.reply_markup);
+      bot.editMessageText(send)
         .catch(err => console.log(err.error.description));
     });
 });
@@ -37,25 +37,25 @@ eventEmitter.on(strings.askRateFreelancerInline, ({ msg, bot }) => {
  * @param  {Telegram:Messager} msg Message received
  */
 eventEmitter.on(strings.askRateClientInline, ({ msg, bot }) => {
-    const jobId = msg.data.split(strings.inlineSeparator)[1];
-    const freelancerId = msg.data.split(strings.inlineSeparator)[2];
+  const jobId = msg.data.split(strings.inlineSeparator)[1];
+  const freelancerId = msg.data.split(strings.inlineSeparator)[2];
 
-    dbmanager.findJobById(jobId, 'client freelancer_chat_inlines')
+  dbmanager.findJobById(jobId, 'client freelancer_chat_inlines')
     .then(job => {
-        dbmanager.findUserById(freelancerId)
+      dbmanager.findUserById(freelancerId)
         .then(freelancer => {
-            let keyboard = keyboards.rateKeyboard(strings.rateFreelancerInline, job._id);
-            const chatInline = dbmanager.chatInline(job, freelancer);
-            let send = {
-                chat_id: chatInline.chat_id,
-                message_id: chatInline.message_id,
-                text: strings.rateClientMessage,
-                reply_markup: {
-                    inline_keyboard: keyboard
-                }
-            };
-            send.reply_markup = JSON.stringify(send.reply_markup);
-            bot.editMessageText(send)
+          let keyboard = keyboards.rateKeyboard(strings.rateFreelancerInline, job._id);
+          const chatInline = dbmanager.chatInline(job, freelancer);
+          let send = {
+            chat_id: chatInline.chat_id,
+            message_id: chatInline.message_id,
+            text: strings.rateClientMessage,
+            reply_markup: {
+              inline_keyboard: keyboard
+            }
+          };
+          send.reply_markup = JSON.stringify(send.reply_markup);
+          bot.editMessageText(send)
             .catch(err => console.log(err.error.description));
         });
     });
@@ -67,14 +67,14 @@ eventEmitter.on(strings.askRateClientInline, ({ msg, bot }) => {
  * @param  {Telegram:Messager} msg Message received
  */
 eventEmitter.on(strings.rateFreelancerInline, ({ msg, bot }) => {
-    const rating = msg.data.split(strings.inlineSeparator)[1];
-    const jobId = msg.data.split(strings.inlineSeparator)[2];
+  const rating = msg.data.split(strings.inlineSeparator)[1];
+  const jobId = msg.data.split(strings.inlineSeparator)[2];
 
-    dbmanager.findJobById(jobId, 'client freelancer_chat_inlines')
+  dbmanager.findJobById(jobId, 'client freelancer_chat_inlines')
     .then(job => {
-        dbmanager.findUserById(job.selectedCandidate)
+      dbmanager.findUserById(job.selectedCandidate)
         .then(freelancer => {
-            writeReview(bot, job, rating, job.client, freelancer, strings.reviewTypes.byFreelancer);
+          writeReview(bot, job, rating, job.client, freelancer, strings.reviewTypes.byFreelancer);
         });
     });
 });
@@ -85,57 +85,66 @@ eventEmitter.on(strings.rateFreelancerInline, ({ msg, bot }) => {
  * @param  {Telegram:Messager} msg Message received
  */
 eventEmitter.on(strings.rateClientInline, ({ msg, bot }) => {
-    const rating = msg.data.split(strings.inlineSeparator)[1];
-    const jobId = msg.data.split(strings.inlineSeparator)[2];
+  const rating = msg.data.split(strings.inlineSeparator)[1];
+  const jobId = msg.data.split(strings.inlineSeparator)[2];
 
-    dbmanager.findJobById(jobId, 'client freelancer_chat_inlines')
+  dbmanager.findJobById(jobId, 'client freelancer_chat_inlines')
     .then(job => {
-        dbmanager.findUserById(job.selectedCandidate)
+      dbmanager.findUserById(job.selectedCandidate)
         .then(freelancer => {
-            writeReview(bot, job, rating, job.client, freelancer, strings.reviewTypes.byClient);
+          writeReview(bot, job, rating, job.client, freelancer, strings.reviewTypes.byClient);
         });
     });
 });
 
+/**
+ * Adds review to client and freelancer user objects; clears message on user that rated
+ * @param  {Telegram:Bot} bot        Bot that should respond
+ * @param  {Mongoose:Job} job        Job about which the report is made
+ * @param  {Number} rating     Rating 1-5 stars
+ * @param  {Mongoose:User} client     Relevant client
+ * @param  {Mongoose:User} freelancer Relevant freelancer
+ * @param  {String} reviewType Type of review (client-freelancer or freelancer-client)
+ */
 function writeReview(bot, job, rating, client, freelancer, reviewType) {
-    const byClient = (reviewType === strings.reviewTypes.byClient);
-    const byUser = (byClient) ? client : freelancer;
-    const toUser = (byClient) ? freelancer : client;
-    const chatInline = dbmanager.chatInline(job, freelancer);
-    const chat_id = (byClient) ? job.current_inline_chat_id : chatInline.chat_id;
-    const message_id = (byClient) ? job.current_inline_message_id : chatInline.message_id;
+  const byClient = (reviewType === strings.reviewTypes.byClient);
+  const byUser = (byClient) ? client : freelancer;
+  const toUser = (byClient) ? freelancer : client;
+  const chatInline = dbmanager.chatInline(job, freelancer);
+  const chat_id = (byClient) ? job.current_inline_chat_id : chatInline.chat_id;
+  const message_id = (byClient) ? job.current_inline_message_id : chatInline.message_id;
 
-    dbmanager.addReview({
-        byUser: byUser,
-        toUser: toUser,
-        job: job._id,
-        rate: rating,
-        review: '',
-        reviewType: reviewType
-    })
+  dbmanager.addReview({
+    byUser: byUser,
+    toUser: toUser,
+    job: job._id,
+    rate: rating,
+    review: '',
+    reviewType: reviewType
+  })
     .then(dbReviewObject => {
-        dbmanager.findUserById(toUser)
+      dbmanager.findUserById(toUser)
         .then(toUser => {
-            toUser.reviews.push(dbReviewObject._id);
-            toUser.save()
+          toUser.reviews.push(dbReviewObject._id);
+          toUser.save()
             .then(toUser => {
-                //todo: Send a message stating that you have received a review
+              // todo: Send a message stating that you have received a review
             });
         });
 
-        byUser.writeReview.push(dbReviewObject._id);
-        byUser.save()
+      byUser.writeReview.push(dbReviewObject._id);
+      byUser.save()
         .then(byUser => {
-            let send = {
-                chat_id: chat_id,
-                message_id: message_id,
-                text: strings.thanksReviewMessage,
-                reply_markup: {
-                    inline_keyboard: []
-                }
-            };
-            send.reply_markup = JSON.stringify(send.reply_markup);
-            bot.editMessageText(send)
+          let send = {
+            chat_id: chat_id,
+            message_id: message_id,
+            text: strings.thanksReviewMessage,
+            reply_markup: {
+              inline_keyboard: []
+            }
+          };
+          send.reply_markup = JSON.stringify(send.reply_markup);
+          bot.editMessageText(send)
             .catch(err => console.log(err.error.description));
         });
     });
