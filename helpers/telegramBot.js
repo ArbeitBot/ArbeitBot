41a57b2f855c031e298b778def9711607e2ee911
backmeupplz <backmeupplz@gmail.com>
2016-08-13 21:14:25 -0700

    add bot message handler

--- helpers/telegramBot.js
+++ helpers/telegramBot.js
@@ -16,6 +16,35 @@ const bot = new Telegram({
   }
 });
 
+function handleBotMessage(item) {
+  // Update events
+  bot.emit('update', item);
+
+  // Inline events
+  if (item.callback_query) {
+      bot.emit('inline.callback.query', item.callback_query);
+  }
+
+  if (item.edited_message) {
+      bot.emit('edited.message', item.edited_message);
+  }
+
+  // On inline query is received
+  if(item.inline_query)
+  {
+      bot.emit('inline.query', item.inline_query);
+  }
+
+  // On inline result is chosen
+  if(item.chosen_inline_result)
+  {
+      bot.emit('inline.result', item.chosen_inline_result);
+  }
+
+  // Notify subscriber
+  bot.emit('message', item.message);
+}
+
 if (config.should_use_webhooks) {
   // Start http server for webhooks
   const options = {
@@ -25,7 +54,7 @@ if (config.should_use_webhooks) {
   https.createServer(options, (req, res) => {
     if (String(req.url) === `/${config.webhook_token}`) {
       body(req, res, (err, body) => {
-        console.log(body);
+        handleBotMessage(body);
       });
       res.writeHead(200);
       res.end();
