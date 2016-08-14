9a7a07a5e232aaa40a1067d8b24626412fd88a24
backmeupplz <backmeupplz@gmail.com>
2016-08-13 21:17:51 -0700

    finish webhook integration

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ module.exports = {
   staging_url: process.env.ARBEIT_BOT_STAGING_URL,
   production_url: process.env.ARBEIT_BOT_PRODUCTION_URL,
   should_use_webhooks: true,
+  // todo: hide from general public before open sourcing
   webhook_token: 'D83Lw8AXaW793xup1Sxj9j6wR6kE7sJj',
   webhook_callback_url: 'https://138.68.6.70:8443/'
 };
