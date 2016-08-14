3d4125164d8ce9fe709ec186f948e38a188dc0da
backmeupplz <backmeupplz@gmail.com>
2016-08-13 20:18:57 -0700

    changed smth

--- helpers/telegramBot.js
+++ helpers/telegramBot.js
@@ -21,7 +21,6 @@ if (config.should_use_webhooks) {
     key: fs.readFileSync(path.join(__dirname, '/../certificates/key.key')),
     cert: fs.readFileSync(path.join(__dirname, '/../certificates/crt.pem'))
   };
-  console.log(options);
   https.createServer(options, (req, res) => {
     if (String(req.url) === `/${config.webhook_token}`) {
       console.log('=======================');
@@ -43,6 +42,8 @@ if (config.should_use_webhooks) {
   });
 
   const pathToCertificate = path.join(__dirname, '/../certificates/crt.pem');
+  console.log(pathToCertificate);
+  console.log(`${config.webhook_callback_url}${config.webhook_token}`);
   bot.setWebhook(`${config.webhook_callback_url}${config.webhook_token}`);
 }
 
