ae97536d26004c8b21ebd0683cb087ff654d4ca0
backmeupplz <backmeupplz@gmail.com>
2016-08-16 11:28:34 -0700

    create webhooks for new telegram api bot

--- helpers/telegramBot.js
+++ helpers/telegramBot.js
@@ -5,73 +5,28 @@
 const Telegram = require('node-telegram-bot-api');
 const config = require('../config');
 const fs = require('fs');
-const https = require('https');
 const path = require('path');
 const body = require('body/json');
 
-const bot = new Telegram(config.telegram_api_key, { 
-  polling: !config.should_use_webhooks 
-});
-
-function handleBotMessage(item) {
-  if (!item) return;
-  // Update events
-  bot.emit('update', item);
-
-  // Inline events
-  if (item.callback_query) {
-      bot.emit('inline.callback.query', item.callback_query);
-  }
-
-  if (item.edited_message) {
-      bot.emit('edited.message', item.edited_message);
-  }
-
-  // On inline query is received
-  if(item.inline_query)
-  {
-      bot.emit('inline.query', item.inline_query);
-  }
-
-  // On inline result is chosen
-  if(item.chosen_inline_result)
-  {
-      bot.emit('inline.result', item.chosen_inline_result);
-  }
-
-  // Notify subscriber
-  bot.emit('message', item.message);
-}
+let bot;
 
 if (config.should_use_webhooks) {
-  // Start http server for webhooks
-  const options = {
-    key: fs.readFileSync(path.join(config.ssl_key_path)),
-    cert: fs.readFileSync(path.join(config.ssl_certificate_path))
-  };
-  https.createServer(options, (req, res) => {
-    if (String(req.url) === `/${config.webhook_token}`) {
-      body(req, res, (err, body) => {
-        handleBotMessage(body);
-      });
-      res.writeHead(200);
-      res.end();
-    } else {
-      res.writeHead(404);
-      res.end('404');
-    }
-  }).listen(8443, () => {
-    console.log('Server for Telegram web hooks listening on: 8443');
+  bot = new Telegram(config.telegram_api_key, { 
+      webHook: {
+        port: 8443,
+        key: config.ssl_key_path,
+        cert: config.ssl_certificate_path
+      }
   });
-  const pathToCertificate = path.join(config.ssl_certificate_path);
-  bot.setWebHook({
-    url: `${config.webhook_callback_url}${config.webhook_token}`, 
-    certificate: pathToCertificate
-  }).then(data => console.log('Telegram webhook is active'))
+  bot.setWebHook(`${config.webhook_callback_url}${config.webhook_token}`, config.ssl_certificate_path)
+    .then(data => console.log('Telegram webhook is active'))
 } else {
+  bot = new Telegram(config.telegram_api_key, { 
+    polling: true
+  });
   bot.setWebHook({
     url: ''
-  })
+  });
   console.log('Telegram is using updates instead of webhooks')
 }
 
