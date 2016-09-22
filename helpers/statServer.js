1ef5498452e9e82cd6544359fc4187c63cded5cb
backmeupplz <backmeupplz@gmail.com>
2016-09-22 12:27:18 -0700

    add CORS headers to stat server and apply airbnb style to statServer.js

--- helpers/statServer.js
+++ helpers/statServer.js
@@ -6,26 +6,28 @@ const http = require('http');
 const dbmanager = require('./dbmanager');
 
 http.createServer((req, res) => {
-if (String(req.url) === `/getStats`) {
-  dbmanager.userCount()
-  	.then(c => {
-      return dbmanager.jobCount()
-        .then(j => {
-          return dbmanager.freelancerCount()
-            .then(f => {
-              res.writeHead(200);
-              res.end(`{ "userCount": ${c}, "jobCount": ${j}, "freelancerCount": ${f} }`);
-            });
-        });
-  	})
-  	.catch(err => {
-  		res.writeHead(500);
-  		res.end(err);
-  	});
-} else {
-  res.writeHead(404);
-  res.end('404');
-}
+  if (String(req.url) === '/getStats') {
+    dbmanager.userCount()
+      .then(c =>
+        dbmanager.jobCount()
+          .then(j =>
+            dbmanager.freelancerCount()
+              .then((f) => {
+                res.writeHead(200);
+                res.header('Access-Control-Allow-Origin', '*');
+                res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+                res.end(`{ "userCount": ${c}, "jobCount": ${j}, "freelancerCount": ${f} }`);
+              })
+          )
+      )
+      .catch((err) => {
+        res.writeHead(500);
+        res.end(err);
+      });
+  } else {
+    res.writeHead(404);
+    res.end('404');
+  }
 }).listen(3000, () => {
-	console.log('Statistics server listening on: 3000');
+  console.log('Statistics server listening on: 3000');
 });
