d08414e0ded8e366f4df62b3fb67194c8cbdb1f2
Denis Maslennikov <nof1000@mail.ru>
2016-10-29 02:30:04 +0300

    Refactored helpers/profile.js

--- helpers/profile.js
+++ helpers/profile.js
@@ -1,87 +1,92 @@
-const dbmanager = require('./dbmanager');
-const strings = require('./strings');
-const keyboards = require('./keyboards');
-const adminReports = require('./adminReports');
+/**
+ * @module helpers/profile
+ * @license MIT
+ */
 
+/** Dependencies */
+const adminReports = require('./adminReports');
+const dbmanager = require('./dbmanager');
+const keyboards = require('./keyboards');
+const strings = require('./strings');
 
 eventEmitter.on(strings.inputBioState, ({ msg, user, bot }) => {
-  let newBio = msg.text.substring(0, 150);
-
-  let needsCongrats = !user.bio && !!user.hourly_rate && user.categories.length > 0;
+  const needsCongrats = !user.bio && !!user.hourly_rate && user.categories.length > 0;
+  const newBio = msg.text.substring(0, 150);
 
   user.bio = newBio;
   user.input_state = undefined;
   user.save()
-    .then(user => {
-      bot.sendMessage(msg.chat.id, strings.changedBioMessage+user.bio, {
+    .then((user) => {
+      bot.sendMessage(msg.chat.id, strings.changedBioMessage + user.bio, {
         reply_markup: JSON.stringify({
           keyboard: keyboards.freelancerKeyboard(user),
-          resize_keyboard: true
+          resize_keyboard: true,
         }),
-        disable_web_page_preview: 'true'
+        disable_web_page_preview: 'true',
       })
-        .then(data => {
+        .then((data) => {
           if (needsCongrats) {
             keyboards.sendKeyboard(
               bot,
               user.id,
               strings.filledEverythingMessage,
-              keyboards.freelancerKeyboard(user));
+              keyboards.freelancerKeyboard(user)
+            );
           }
         })
-        .catch(err => console.error(err.message));
+        .catch((err) => { console.error(err.message); });
     });
 });
 
 eventEmitter.on('cancel' + strings.inputBioState, ({ msg, user, bot }) => {
   user.input_state = undefined;
   user.save()
-    .then(user => {
-      bot.sendMessage(msg.chat.id, strings.notChangedBioMessage+user.bio, {
+    .then((user) => {
+      bot.sendMessage(msg.chat.id, strings.notChangedBioMessage + user.bio, {
         reply_markup: JSON.stringify({
           keyboard: keyboards.freelancerKeyboard(user),
-          resize_keyboard: true
+          resize_keyboard: true,
         }),
-        disable_web_page_preview: 'true'
-      })
-        .catch(err => console.error(err.message));
+        disable_web_page_preview: 'true',
+      }).catch((err) => { console.error(err.message); });
     });
 });
 
-
 function createProfile(bot, msg) {
   dbmanager.addUser(msg.from)
-    .then(obj => {
+    .then((obj) => {
       const user = obj.user;
       const isNew = obj.new;
-      if (isNew) {
-        adminReports.userRegistered(bot, user);
-      }
+
+      if (isNew) adminReports.userRegistered(bot, user);
       keyboards.sendMainMenu(bot, msg.chat.id, true);
     });
 }
 
 /**
  * Sends message to user asking for bio and adds relevant flags to user's object
- * @param  {Telegram:Message} msg Message received
- * @param  {Telegram:Bot} bot Bot that should respond
+ *
+ * @param {Telegram:Message} msg - Message received
+ * @param {Telegram:Bot} bot - Bot that should respond
  */
 function askForBio(msg, bot) {
   dbmanager.findUser({ id: msg.chat.id })
-    .then(user => {
+    .then((user) => {
       user.input_state = strings.inputBioState;
       user.save()
-        .then(user => {
-          let message = user.bio ?
-          strings.editBioMessage+'\n\n'+strings.yourCurrentBio+'\n\n'+user.bio : strings.editBioMessage;
-         
+        .then((user) => {
+          const message = ((user.bio) ?
+            strings.editBioMessage + '\n\n' + strings.yourCurrentBio + '\n\n' + user.bio :
+            strings.editBioMessage
+          );
+
           bot.sendMessage(msg.chat.id, message, {
             reply_markup: JSON.stringify({
-              keyboard: [[ strings.cancel ]],
-              resize_keyboard: true
+              keyboard: [[strings.cancel]],
+              resize_keyboard: true,
             }),
-            disable_web_page_preview: 'true'
-          }).catch(err => console.error(err.message));
+            disable_web_page_preview: 'true',
+          }).catch((err) => { console.error(err.message); });
         });
     });
 }
@@ -91,47 +96,53 @@ function updateProfile(msg, user) {
     user.first_name = msg.from.first_name;
     user.last_name = msg.from.last_name;
     user.username = msg.from.username;
-    
+
     user.save()
-      .catch(err => console.error(err.message));
+      .catch((err) => { console.error(err.message); });
   }
 }
 
 /**
- * Toggles user 'busy' status – if it was true, makes it false and vice versa; sends freelancer menu afterwards
- * @param  {Telegram:Bot} bot Bot that should respond
- * @param {Number} chatId Chat id of user who should have his busy status toggled
+ * Toggles user 'busy' status – if it was true, makes it false and vice versa;
+ * Sends freelancer menu afterwards.
+ *
+ * @param {Telegram:Bot} bot - Bot that should respond
+ * @param {Number} chatId - Chat id of user who should have his busy status toggled
  */
 function toggleAvailability(bot, chatId) {
   dbmanager.toggleUserAvailability(chatId)
-    .then(user => {
-      let message = user.busy ? strings.becameBusyMessage : strings.becameAvailableMessage;
-     
+    .then((user) => {
+      let message = ((user.busy) ?
+        strings.becameBusyMessage :
+        strings.becameAvailableMessage
+      );
+
       if (!user.bio || user.categories.length <= 0 || !user.hourly_rate) {
-        message = user.busy ? strings.missingBecameBusyMessage : strings.missingBecameAvailableMessage;
+        message = ((user.busy) ?
+          strings.missingBecameBusyMessage :
+          strings.missingBecameAvailableMessage
+        );
       }
-      
+
       keyboards.sendKeyboard(bot, chatId, message, keyboards.freelancerKeyboard(user));
     });
 }
 
 
-// Helpers
+/** Helpers */
 
 /**
  * Checks if state of user that sent message is one of input ones
- * @param  {Telegram:Messahe}   msg      Message received
- * @param  {Function} callback Callback(input_state, user) that is called when check is done
+ *
+ * @param {Telegram:Messahe} msg - Message received
+ * @param {Function} callback - Callback(input_state, user) that is called when check is done
  */
 function textInputCheck(msg, callback) {
   dbmanager.findUser({ id: msg.chat.id })
-    .then(user => {
-      if (user) {
-        callback(user.input_state, user);
-      } else {
-        callback();
-      }
-    })
+    .then((user) => {
+      if (user) callback(user.input_state, user);
+      else callback();
+    });
 }
 
 function sendAskForUsername(bot, msg) {
@@ -142,7 +153,7 @@ function sendBanMessage(bot, msg) {
   bot.sendMessage(msg.from.id, strings.banMessage);
 }
 
-// Exports
+/** Exports */
 module.exports = {
   createProfile,
   updateProfile,
@@ -150,5 +161,5 @@ module.exports = {
   toggleAvailability,
   textInputCheck,
   sendAskForUsername,
-  sendBanMessage
+  sendBanMessage,
 };
