d4416add30025b130c6b908ae645a3556786abdd
Denis Maslennikov <nof1000@mail.ru>
2016-10-29 00:46:16 +0300

    Refactored helpers/hourlyRatePicker.js

--- helpers/hourlyRatePicker.js
+++ helpers/hourlyRatePicker.js
@@ -1,15 +1,20 @@
 /**
  * Used to send hourly rate picker to freelancers as inlines and to handle answers to those inlines
+ *
+ * @module helpers/hourlyRatePicker
+ * @license MIT
  */
 
-let keyboards = require('./keyboards');
-let dbmanager = require('./dbmanager');
-let strings = require('./strings');
+/** Dependencies */
+const keyboards = require('./keyboards');
+const dbmanager = require('./dbmanager');
+const strings = require('./strings');
 
 /**
  * Handles inline answers from users
- * @param  {Telegram:Bot} bot Bot that should edit or send hourly rate keyboard
- * @param  {Telegram:Message} msg Message that came along with inline button click
+ *
+ * @param  {Telegram:Bot} bot - Bot that should edit or send hourly rate keyboard
+ * @param  {Telegram:Message} msg - Message that came along with inline button click
  */
 eventEmitter.on(strings.hourlyRateInline, ({ msg, bot }) => {
   editHourlyRate(bot, msg);
@@ -17,89 +22,99 @@ eventEmitter.on(strings.hourlyRateInline, ({ msg, bot }) => {
 
 /**
  * Sends initial message with hourly rate picker inline
- * @param  {Telegram:Bot} bot Bot that should send message and inline
- * @param  {Number} chatId Chat id of user who should receive inline
+ *
+ * @param  {Telegram:Bot} bot - Bot that should send message and inline
+ * @param  {Number} chatId - Chat id of user who should receive inline
  */
 function sendHourlyRate(bot, chatId) {
   dbmanager.findUser({ id: chatId })
-    .then(user => {
-      let hourlyRates = strings.hourlyRateOptions;
-      let keyboard = hourlyRateKeyboard(user, hourlyRates);
+    .then((user) => {
+      const hourlyRates = strings.hourlyRateOptions;
+      const keyboard = hourlyRateKeyboard(user, hourlyRates);
+
       keyboards.sendInline(
-        bot, 
+        bot,
         user.id,
         strings.editHourlyRateMessage,
-        keyboard);
+        keyboard
+      );
     });
 }
 
 /**
  * Used to edit existing message with inline of user who has changed his hourly rate
- * @param  {Telegram:Bot} bot Bot that should edit message
- * @param  {Telegram:Message} msg Message that came along with inline button click
+ *
+ * @param  {Telegram:Bot} bot - Bot that should edit message
+ * @param  {Telegram:Message} msg - Message that came along with inline button click
  */
 function editHourlyRate(bot, msg) {
   let command = msg.data.split(strings.inlineSeparator)[1];
 
   function getUserCallback(user) {
-
+    /** @todo: WHAT? Fix it. */
   }
 
   dbmanager.findUser({ id: msg.message.chat.id })
-    .then(user => {
-      let needCongrats = !user.hourly_rate;
+    .then((user) => {
+      const needCongrats = !user.hourly_rate;
       user.hourly_rate = command;
       user.save()
-        .then(user => {
+        .then((user) => {
           keyboards.editInline(
             bot,
             msg.message.chat.id,
             msg.message.message_id,
-            hourlyRateKeyboard(user, strings.hourlyRateOptions));
+            hourlyRateKeyboard(user, strings.hourlyRateOptions)
+          );
+
           if (needCongrats && user.bio && user.categories.length > 0) {
             keyboards.sendKeyboard(
               bot,
-              user.id, 
-              strings.filledEverythingMessage, 
-              keyboards.freelancerKeyboard(user));
+              user.id,
+              strings.filledEverythingMessage,
+              keyboards.freelancerKeyboard(user)
+            );
           }
       });
     });
 }
 
 /**
- * Gets hourly rate inline keyboard for freelancer; highlights hourly rate that is currently selected
- * @param  {Mongoose:User} user User that should receive keyboard later
- * @param  {[String]} hourlyRates A list of all hourly rates that should be shown to user
+ * Gets hourly rate inline keyboard for freelancer;
+ * Highlights hourly rate that is currently selected.
+ *
+ * @param  {Mongoose:User} user - User that should receive keyboard later
+ * @param  {[String]} hourlyRates - A list of all hourly rates that should be shown to user
  * @return {Telegram:Inline} Inline keyboard that gets created depending on user's picked hourly rate
  */
 function hourlyRateKeyboard(user, hourlyRates) {
   let hourlyRate = user.hourly_rate;
-
   let keyboard = [];
   let tempRow = [];
-  for (var i in hourlyRates) {
-    let isOdd = i % 2 == 1;
-    let currentHR = hourlyRates[i];
 
-    let text = hourlyRate == currentHR ?
-      strings.selectedHourlyRate+currentHR :
+  for (var i in hourlyRates) {
+    const isOdd = i % 2 == 1;
+    const currentHR = hourlyRates[i];
+
+    const text = hourlyRate == ((currentHR) ?
+      strings.selectedHourlyRate + currentHR :
       currentHR
+    );
 
     tempRow.push({
       text: text,
-      callback_data: strings.hourlyRateInline+strings.inlineSeparator+currentHR
+      callback_data: strings.hourlyRateInline + strings.inlineSeparator + currentHR,
     });
+
     if (isOdd) {
       keyboard.push(tempRow);
       tempRow = [];
     }
   }
+
   return keyboard;
 }
 
-// Exports
-
 module.exports = {
-  sendHourlyRate: sendHourlyRate
+  sendHourlyRate: sendHourlyRate,
 };
