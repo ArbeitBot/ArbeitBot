047882f5ecc8f1db2c3e52f8498d937ac9a0ad21
Denis Maslennikov <nof1000@mail.ru>
2016-10-28 20:28:52 +0300

    Refactored helpers/categoryPicker.js

--- helpers/categoryPicker.js
+++ helpers/categoryPicker.js
@@ -1,7 +1,11 @@
 /**
  * Category picker is a file that manages category picking for freelancers through inline keyboards
+ *
+ * @module helpers/categoryPicker
+ * @license MIT
  */
 
+/** Dependencies */
 const keyboards = require('./keyboards');
 const dbmanager = require('./dbmanager');
 const strings = require('./strings');
@@ -10,16 +14,18 @@ const pageSize = 10;
 
 /**
  * Handles incoming message that's send when user selects an inline button
- * @param  {Telegram:Bot} bot Bot that should send a response to this action
- * @param  {Telegram:Message} msg Message that was received upon clicking an inline button
+ *
+ * @param  {Telegram:Bot} bot - Bot that should send a response to this action
+ * @param  {Telegram:Message} - msg Message that was received upon clicking an inline button
  */
 eventEmitter.on(strings.categoryInline, ({ msg, bot }) => {
   const command = msg.data.split(strings.inlineSeparator)[1];
-  const page = parseInt(msg.data.split(strings.inlineSeparator)[2]);
+  const page = parseInt(msg.data.split(strings.inlineSeparator)[2], 10);
+
   if (command === strings.categoryLeft) {
-    editPage(bot, msg, page-1);
+    editPage(bot, msg, page - 1);
   } else if (command === strings.categoryRight) {
-    editPage(bot, msg, page+1);
+    editPage(bot, msg, page + 1);
   } else {
     dbmanager.toggleCategoryForUser(msg.message.chat.id, command)
       .then(({ user, isAdded }) => {
@@ -27,11 +33,13 @@ eventEmitter.on(strings.categoryInline, ({ msg, bot }) => {
           if (user.bio && user.hourly_rate && user.categories.length === 1) {
             keyboards.sendKeyboard(
               bot,
-              user.id, 
-              strings.filledEverythingMessage, 
-              keyboards.freelancerKeyboard(user));
+              user.id,
+              strings.filledEverythingMessage,
+              keyboards.freelancerKeyboard(user)
+            );
           }
         }
+
         editPage(bot, msg, page);
       });
   }
@@ -39,14 +47,15 @@ eventEmitter.on(strings.categoryInline, ({ msg, bot }) => {
 
 /**
  * Sends freelancer list of categories with inline buttons to pick categories
- * @param  {Telegram:Bot} bot Bot that should send keyboard
- * @param  {Number} chatId Chat id of user that should receive keyboard
+ *
+ * @param  {Telegram:Bot} bot - Bot that should send keyboard
+ * @param  {Number} chatId - Chat id of user that should receive keyboard
  */
 function sendCategories(bot, chatId) {
   dbmanager.findUser({ id: chatId })
-    .then(user => {
+    .then((user) => {
       dbmanager.getCategories()
-        .then(categories => {
+        .then((categories) => {
           getCategoriesCallback(categories, user, bot);
         });
     });
@@ -54,79 +63,87 @@ function sendCategories(bot, chatId) {
 
 /**
  * Callback function that is used when user and categories are obtained from Mongo DB – triggers sending of categories picker to user
- * @param  {[Mongoose:Category]} categories A list of categories that should be available for picking
- * @param  {Mongoose:User} user User that should receive a keyboard
- * @param  {Telegram:Bot} bot Bot that should send a keyboard
+ *
+ * @param  {[Mongoose:Category]} categories - A list of categories that should be available for picking
+ * @param  {Mongoose:User} user - User that should receive a keyboard
+ * @param  {Telegram:Bot} bot - Bot that should send a keyboard
  */
 function getCategoriesCallback(categories, user, bot) {
   const keyboard = categoriesKeyboard(categories, user, 0);
+
   keyboards.sendInline(
-    bot, 
+    bot,
     user.id,
     strings.pickCategoriesMessage,
-    keyboard);
+    keyboard
+  );
 }
 
 /**
  * Edits message and it's inline buttons for category picker message – mainly used for paging and if we need to update message with category picker
- * @param  {Telegram:Bot} bot Bot that should edit message
- * @param  {Telegram:Message} msg Message that should be editted, usually obtained with inline button click callback
- * @param  {Number} page Page of the list of categories that should be displayed
+ *
+ * @param  {Telegram:Bot} bot - Bot that should edit message
+ * @param  {Telegram:Message} msg - Message that should be editted, usually obtained with inline button click callback
+ * @param  {Number} page - Page of the list of categories that should be displayed
  */
 function editPage(bot, msg, page) {
   dbmanager.findUser({ id: msg.message.chat.id })
-    .then(user => {
+    .then((user) => {
       dbmanager.getCategories()
-        .then(categories => {
+        .then((categories) => {
           keyboards.editInline(
             bot,
             msg.message.chat.id,
             msg.message.message_id,
-            categoriesKeyboard(categories, user, page));
+            categoriesKeyboard(categories, user, page)
+          );
         });
     });
 }
 
 /**
  * Returns inline keyboard for freelancer; decorates it with checkmark if category is picked, supports multiple categories to be picked; shows picked categories on top of the list; supports paging
- * @param  {[Mongoose:Category]} categories A list of all categories that should be shown
- * @param  {Mongoose:User} user User that requested keyboards
- * @param  {Number} page Page for which the keyboard should be sent; i.e. page 1 will return categories 0 through pageSize, page 2 will return pages pageSize through pageSize*2
+ *
+ * @param  {[Mongoose:Category]} categories - A list of all categories that should be shown
+ * @param  {Mongoose:User} user - User that requested keyboards
+ * @param  {Number} page - Page for which the keyboard should be sent; i.e. page 1 will return categories 0 through pageSize, page 2 will return pages pageSize through pageSize*2
  * @return {Telegram:InlineKeyboard} Keyboard with paging buttons if required and a list of categories with picked indicator for specified user
  */
 function categoriesKeyboard(categories, user, page) {
   let categoriesLeft = [];
+
   for (let i in categories) {
     const cat = categories[i];
     let shouldAdd = true;
+
     for (let j in user.categories) {
       let inCat = user.categories[j];
-      if (''+inCat._id === ''+cat._id) {
-        shouldAdd = false;
-      }
+      if ('' + inCat._id === '' + cat._id) shouldAdd = false;
     }
-    if (shouldAdd) {
-      categoriesLeft.push(cat);
-    }
+
+    if (shouldAdd) categoriesLeft.push(cat);
   }
+
   let allCategories = user.categories.concat(categoriesLeft);
-  allCategories = allCategories.slice(page*pageSize,page*pageSize+pageSize);
+  allCategories = allCategories.slice(page * pageSize, page * (pageSize + pageSize));
 
   let keyboard = [];
   let tempRow = [];
   for (let i in allCategories) {
-    const isOdd = i % 2 === 1;
+    const isOdd = (i % 2) === 1;
     const isLast = i == allCategories.length - 1;
     const currentCategory = allCategories[i];
 
-    const text = user.categories.indexOf(currentCategory) > -1 ?
-      strings.selectedCategory+currentCategory.title :
-      currentCategory.title;
+    const text = ((user.categories.indexOf(currentCategory) > -1) ?
+      strings.selectedCategory + currentCategory.title :
+      currentCategory.title
+    );
 
     tempRow.push({
       text: text,
-      callback_data: strings.categoryInline+strings.inlineSeparator+currentCategory._id+strings.inlineSeparator+page
+      callback_data: strings.categoryInline + strings.inlineSeparator + currentCategory._id + strings.inlineSeparator + page
     });
+
     if (isOdd || isLast) {
       keyboard.push(tempRow);
       tempRow = [];
@@ -137,32 +154,42 @@ function categoriesKeyboard(categories, user, page) {
   if (page > 0) {
     navButtons.push({
       text: strings.categoryLeft,
-      callback_data: strings.categoryInline+strings.inlineSeparator+strings.categoryLeft+strings.inlineSeparator+page
+      callback_data: strings.categoryInline +
+        strings.inlineSeparator +
+        strings.categoryLeft +
+        strings.inlineSeparator +
+        page
     });
   }
+
   const remainder = categories.length % pageSize;
   let lastPage = (categories.length - remainder) / pageSize;
-  if (remainder > 0) {
-    lastPage = lastPage + 1;
-  }
+
+  if (remainder > 0) lastPage = lastPage + 1;
   if (page+1 < lastPage) {
     navButtons.push({
       text: strings.categoryRight,
-      callback_data: strings.categoryInline+strings.inlineSeparator+strings.categoryRight+strings.inlineSeparator+page
+      callback_data: strings.categoryInline +
+        strings.inlineSeparator +
+        strings.categoryRight +
+        strings.inlineSeparator +
+        page
     });
   }
+
   keyboard.push(navButtons);
-  if (page+1 === lastPage) {
-    keyboard.push([{ 
+
+  if (page + 1 === lastPage) {
+    keyboard.push([{
       text: strings.suggestCategoryMessage,
-      url: 'http://telegram.me/borodutch' 
+      url: 'http://telegram.me/borodutch'
     }]);
   }
+
   return keyboard;
 }
 
-// Exports
-
+/** Exports */
 module.exports = {
   sendCategories
 };
