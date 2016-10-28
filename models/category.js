bd418db6409c1582ff2a6c36dbb4b4712f6f8956
Denis Maslennikov <nof1000@mail.ru>
2016-10-28 18:53:12 +0300

    Refactored models/category.js

--- models/category.js
+++ models/category.js
@@ -1,14 +1,20 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+/**
+ * @module models/category
+ * @license MIT
+ */
 
+/** Dependencies */
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
 const categorySchema = new Schema({
   title: String,
   freelancers: [{
     type: Schema.ObjectId,
     ref: 'user',
     required: true,
-    default: []
-  }]
+    default: [],
+  }],
 });
 
 mongoose.model('category', categorySchema);
