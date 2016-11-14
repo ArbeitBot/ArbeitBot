9f578869074d144b76f4d9d2c379e8ca4294466e
ALEXZZZ9 <alex@ax9.me>
2016-11-14 16:30:59 +0300

    bugfix

--- helpers/adminNotifications.js
+++ helpers/adminNotifications.js
@@ -59,10 +59,12 @@ function adminNewReviewKeyboard(review) {
  * @return {String}  Text generated for admins from the given review
  */
 function formNewReviewMessageText(review) {
-  const status = (String(review.byUser._id) === String(review.job.client)) ? 'client' : 'freelancer';
+  function status(userId) {
+    return (String(userId) === String(review.job.client)) ? 'client' : 'freelancer';
+  }
   const message =
     `${strings.rateOptionsArray[review.rate - 1]}\n` +
-    `@${review.byUser.username} (${status}) rated @${review.toUser.username} (${status})\n` +
+    `@${review.byUser.username} (${status(review.byUser._id)}) rated @${review.toUser.username} (${status(review.toUser._id)})\n` +
     `${review.resolveWay || ''}\n` +
     `${review.job.description}`;
 
