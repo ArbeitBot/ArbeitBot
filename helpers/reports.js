900b45fd778ae1848e21b51181c4c26ed09aa648
Михаил Ерохов <kingmixaileroxov@gmail.com>
2016-12-18 10:29:49 +0300

    add info about report after ban

--- helpers/reports.js
+++ helpers/reports.js
@@ -146,13 +146,18 @@ function deleteAllAdminMessages(report, bot) {
     const chatId = msgData[1];
     const messageId = msgData[0];
     const emptyKeyboard = [];
+    
+    formReportMessage(report)
+      .then((message) => {
 
-    /** Mark report message as viewed and resolved */
-    bot.editMessageText('Resolved report.', {
-      chat_id: chatId,
-      message_id: messageId,
-    }).catch(/** todo: handle error */);
+        /** Mark report message as viewed and resolved */
+        bot.editMessageText( message + '\nResolved report.', {
+          chat_id: chatId,
+          message_id: messageId,
+        }).catch(/** todo: handle error */);  
 
+      })
+      .catch(/** todo: handle error */);
 
     keyboards.editInline(
       bot,
