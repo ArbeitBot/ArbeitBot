be0a938bb0cacdf2d31342b296940d7d8ef99cb1
Goshan Goshanov <goshaplutahin@gmail.com>
2017-07-18 22:55:14 +0300

    Update reports.js

--- helpers/reports.js
+++ helpers/reports.js
@@ -11,7 +11,7 @@ const strings = require('./strings');
 
 const Report = mongoose.model('report');
 
-const admins = ['74169393', '-1001052392095'];
+const admins = ['-1001052392095'];
 
 /** Inlines */
 
