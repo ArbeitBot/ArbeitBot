d27667e6b08c69487039d7a6c87da72c29accf72
backmeupplz <backmeupplz@gmail.com>
2021-11-17 06:46:53 -0800

    add unhandled rejection handler

--- app.js
+++ app.js
@@ -7,27 +7,31 @@
  */
 
 /** Dependencies */
-const mongoose = require('mongoose');
-const events = require('events');
+const mongoose = require("mongoose");
+const events = require("events");
 
 // noinspection JSAnnotator
 global.eventEmitter = new events.EventEmitter();
 
 /** Configure a bot */
-const config = require('./config');
+const config = require("./config");
 
 /** Setup mongoose */
 mongoose.Promise = global.Promise;
 mongoose.connect(config.database);
 
 /** Start bot */
-require('./helpers/logic');
+require("./helpers/logic");
 
 /** Start stat server */
-require('./helpers/statServer');
+require("./helpers/statServer");
 
 /** Start timers for retention boost and reminders */
-require('./helpers/timers').startTimers();
+require("./helpers/timers").startTimers();
 
 /** Print a message to assure that bot is up and running */
-console.log('Bot is up and running'); // eslint-disable-line no-console
+console.log("Bot is up and running"); // eslint-disable-line no-console
+
+process.on("unhandledRejection", (reason) => {
+  console.log("Unhandled Rejection at:", reason);
+});
